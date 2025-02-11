122d44ddf599a0ee484ada38f36d02cd7345b554
Amirhossein Kalari <hosein.kalari.1387@gmail.com>
2025-02-11 19:03:53 +0330

    db: set up the users table query

--- src/db/tables.ts
+++ src/db/tables.ts
@@ -1,6 +1,18 @@
 import { Database } from "better-sqlite3";
 
 export const createTables = (db: Database) => {
+  // create users table
+  db.exec(`
+    CREATE TABLE IF NOT EXISTS users (
+      id TEXT PRIMARY KEY,
+      username TEXT NOT NULL UNIQUE,
+      email TEXT NOT NULL UNIQUE,
+      password TEXT NOT NULL UNIQUE,
+      createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+    `);
+  
+  // create posts table
   db.exec(`
     CREATE TABLE IF NOT EXISTS posts (
       id TEXT PRIMARY KEY,
