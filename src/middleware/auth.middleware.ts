4c026b8a31032ff1958119c78e6cee8e5a91227e
Amirhossein Kalari <hosein.kalari.1387@gmail.com>
2025-02-11 22:28:12 +0330

    fix: read the authorization token correctly

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -8,7 +8,7 @@ export const authenticationMiddleware = async (
   next: NextFunction
 ) => {
   const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(".")[1];
+  const token = authHeader && authHeader.split(" ")[1];
 
   // check if the token doesn't exist in the request
   if (!token) {
