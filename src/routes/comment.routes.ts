f69421b842715a6695dc0d049f0a953ffd53f913
Amirhossein Kalari <hosein.kalari.1387@gmail.com>
2025-02-12 21:18:57 +0330

    docs: again, remove useless comments

--- src/routes/comment.routes.ts
+++ src/routes/comment.routes.ts
@@ -17,28 +17,19 @@ import { auth } from "../middleware/auth.middleware";
 
 const router = express.Router();
 
-// ensure all the requests are authorized
 router.use(auth);
-
-// Create a new comment
 router.post("/", validateBody(createCommentSchema), createComment);
-
-// Get all comments for a specific post (e.g., GET /comments/post/:post_id)
 router.get(
   "/post/:postId",
   validateParams(getCommentsByPostParamsSchema),
   getCommentsByPost
 );
-
-// Update a comment by its id (e.g., PUT /comments/:id)
 router.put(
   "/:id",
   validateParams(commentParamsSchema),
   validateBody(updateCommentSchema),
   updateComment
 );
-
-// Delete a comment by its id (e.g., DELETE /comments/:id)
 router.delete("/:id", validateParams(commentParamsSchema), deleteComment);
 
 export default router;
