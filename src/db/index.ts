36467e58b9d3a27b4f4c80fc405b548becf28b07
Amirhosein Kalari <hosein.kalari.1387@gmail.com>
2025-04-20 18:02:28 +0330

    wip

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/mysql2";
 
-const db = drizzle(
+export const db = drizzle(
   process.env.DATABASE_URL ?? "mysql://root@localhost:3306/lolo_api"
 );
